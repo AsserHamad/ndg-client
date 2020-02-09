4d8eb69ad149ec206d47d28f830a1a05ccabc7c3
Asser Hamad <asserhamad@gmail.com>
2020-02-09 23:33:47 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("http://localhost:9000/testapi").then(response => response.json()).then(response => console.log(response));
   return (
     <div className="App">
       <header className="App-header">
