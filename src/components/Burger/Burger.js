ec36908f06f96edc26b145ceb74c78144f2f4f8d
Asser Hamad <asserhamad@gmail.com>
2020-02-17 21:07:36 +0200

    styling menu and added icons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,41 +4,42 @@ import { Link } from "react-router-dom";
 import useGlobalState from "../../useGlobalState";
 import './Burger.css';
 
+import { FaHome, FaProjectDiagram, FaInfoCircle, FaServicestack, FaPhone } from 'react-icons/fa';
+
 function Burger(props) {
     const globalState = useGlobalState();
     const lang = globalState.lang.lang;
     const navbar = props.navbar;
-
     return(
         <div id="burger">
             <Menu right>
                 <ul id="menu">
+                    <Link
+                    to="/"
+                    onClick={() => globalState.setPage({ page: "home" })}
+                    style={{ textDecoration: "inherit", fontSize: "inherit" }}
+                    >
+                        <li>
+                            <div className={lang + " menuText"}><div className="icon"><FaHome /></div>{navbar.home}</div>
+                        </li>
+                    </Link>
                     <li>
-                        <Link
-                        to="/"
-                        onClick={() => globalState.setPage({ page: "home" })}
-                        style={{ textDecoration: "inherit", fontSize: "inherit" }}
-                        >
-                        <span className={lang}>{navbar.home}</span>
-                        </Link>
+                        <div className={lang + " menuText"}><div className="icon"><FaProjectDiagram /></div>{navbar.projects}</div>
+                    </li>
+                    <Link
+                    to="/about"
+                    onClick={() => globalState.setPage({ page: "about" })}
+                    style={{ textDecoration: "inherit", fontSize: "inherit" }}
+                    >
+                        <li>
+                        <div className={lang + " menuText"}><div className="icon"><FaInfoCircle /></div>{navbar.about}</div>
+                        </li>
+                    </Link>
+                    <li>
+                        <div className={lang + " menuText"}><div className="icon"><FaServicestack /></div>{navbar.services}</div>
                     </li>
                     <li>
-                        <span className={lang}>{navbar.projects}</span>
-                    </li>
-                    <li>
-                        <Link
-                        to="/about"
-                        onClick={() => globalState.setPage({ page: "about" })}
-                        style={{ textDecoration: "inherit", fontSize: "inherit" }}
-                        >
-                        <span className={lang}>{navbar.about}</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <span className={lang}>{navbar.services}</span>
-                    </li>
-                    <li>
-                        <span className={lang}>{navbar.contact}</span>
+                        <div className={lang + " menuText"}><div className="icon"><FaPhone /></div>{navbar.contact}</div>
                     </li>
                 </ul>
             </Menu>
