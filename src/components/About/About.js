41a9b163cbff091b32eaef7b7e7e13070b2f5b76
Omar <omaar.sherif@gmail.com>
2020-02-24 14:57:27 +0200

    about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,42 @@
-import React, { useEffect } from 'react';
-import './About.css';
+import React, { useState, useEffect } from "react";
+import "./About.css";
+import useGlobalState from "../../useGlobalState";
+import about from "./dummyabout";
 
 function About() {
+  const globalState = useGlobalState(),
+    [aboutText, setaboutText] = useState({}),
+    lang = globalState.lang.lang;
+  useEffect(() => {
+    fetch("/data/lang.json")
+      .then(res => res.json())
+      .then(res => {
+        setaboutText(res[lang].about);
+      });
+  }, [lang]);
   return (
-    <div className="about-container page">
-      <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+    <div className="about-container">
+      <div className="about-us-container">
+        <p className={`wwd wwd-${lang}`}>{aboutText.aboutUs}</p>
+        <div className={`todo-brownies todo-brownies-${lang}`}>
+          <p>{aboutText.corporateOverview}</p>
+        </div>
+      </div>
+      <div className="main-about-container">
+        {about.map(element => (
+          <div className="about-div">
+            <p className={`about-title about-title-${lang}`}>
+              {element.title[lang]}
+            </p>
+            <img src={element.image} />
+            <div className={`list list-${lang}`}>
+              {element.text[lang].map(item => (
+                <li>{item}</li>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/About/dummyabout.js
@@ -0,0 +1,28 @@
+const about = [
+  {
+    title: {
+      en: "ABOUT US",
+      ar: "معلومات عنا"
+    },
+    image:
+      "http://www.naturedesigngroup.com/web_test/public/Uploads/aboutus/image/en/1(1)1555170414.jpg",
+    text: {
+      en: ["about us"],
+      ar: ["معلومات"]
+    }
+  },
+  {
+    title: {
+      en: "MOTIVATIONS",
+      ar: "الدوافع"
+    },
+    image:
+      "http://www.naturedesigngroup.com/web_test/public/Uploads/aboutus/image/en/1(1)1555170414.jpg",
+    text: {
+      en: ["MOTIVATIONS"],
+      ar: ["الدوافع"]
+    }
+  }
+];
+
+export default about;
